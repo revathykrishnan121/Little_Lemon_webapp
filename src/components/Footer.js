e9b842f1472327151fe4900be4e5136e3c00437d
Revathy Krishnan R <revathykrishnan121@gmail.com>
2024-01-13 19:59:04 +0530

    Added meta tags and components

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import logo_footer from "../assets/logo_footer.png"
+function Footer() {
+    return (
+        <footer>
+        <div>
+            <img src={logo_footer} alt="Logo"></img>
+        </div>
+        <div>
+            <p>Copyright Little Lemon</p>
+        </div>
+    </footer>
+    )
+}
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import logo from "../assets/logo.svg"
+function Header() {
+    return (
+    <header>
+         <img src={logo} alt="Logo"></img>
+    </header>
+    )
+}
+export default Header;
